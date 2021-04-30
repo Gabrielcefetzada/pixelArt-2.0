b7602df57b736f4dea3ec10243d70be31f867ffd
Gabriel Augusto <gabrielaugustosb13@gmail.com>
2021-04-30 19:25:54 -0300

    styling the tool bar

--- src/components/PixelBox.tsx
+++ src/components/PixelBox.tsx
@@ -1,46 +1,57 @@
 import React, {useState, useEffect} from 'react';
-import logo from '../images/logo_pixel.png'
-import colorp from '../images/color_picker.png'
 import '../styles/Tool.css'
+import '../styles/global.css'
+import '../styles/colorp.css'
 import { Tool } from './Tool'
 import { ToolProps } from './Tool'
+
+// images
+
+import logo from '../images/logo_pixel.png'
+import colorp from '../images/color_picker.png'
+import  bucket from '../images/balde-de-tinta.png.png'
+import ligth from '../images/iluminador.png'
+import pencil from '../images/lapis.png'
+import bigPencil from '../images/pincel.png'
+import  borrow from '../images/borracha.png'
+import colorpImg from '../images/selecionador-de-cores.png'
 
 export const PixelBox = () => {
 
     const vectorTools = [
         {
             key: 1,
-            photo: logo,
+            photo: bucket,
             tool: "tool1",
         }, 
 
         {
             key: 2,
-            photo: logo,
+            photo: pencil,
             tool: "tool2",
         },
 
         {
             key: 3,
-            photo: logo,
+            photo: borrow,
             tool: "tool3",
         },
 
         {
             key: 4,
-            photo: logo,
+            photo: bigPencil,
             tool: "tool4",
         },
 
         {
             key: 5,
-            photo: logo,
+            photo: colorpImg,
             tool: "tool5",
         },
 
         {
             key: 6,
-            photo: logo,
+            photo: ligth,
             tool: "tool6",
         },
     ];
@@ -55,6 +66,7 @@ export const PixelBox = () => {
     return (
         <div>
             <h1>PixelBox</h1>
+            <h1 className="tool-h1">Ferramentas</h1>
                 <div className="container-tools">
                     {tools.map((tool, index) => (
                         <Tool 
@@ -66,8 +78,10 @@ export const PixelBox = () => {
                             />
                     ))}
                 </div>
-                <div>
-                     <img src={colorp}></img><input type="color"></input>
+                <div className="container-colorp">
+                    <div className="content-white">
+                     <img src={colorp}></img>⠀<input type="color"></input>
+                    </div>
                 </div>
         </div>
     )
